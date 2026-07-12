99776a0c7390ebeb50b3f2aac80433e8dc6f340a
hotaka <s1f102103864@iniad.org>
2026-07-12 17:44:06 +0900

    feat: project create

---
+++ The-Composable-Architecture/The-Composable-Architecture/The_Composable_ArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Composable_ArchitectureApp.swift
+//  The-Composable-Architecture
+//
+//  Created by 金澤帆高 on 2026/07/12.
+//
+
+import SwiftUI
+
+@main
+struct The_Composable_ArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
